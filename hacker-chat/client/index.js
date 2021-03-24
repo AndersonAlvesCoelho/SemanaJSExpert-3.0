c09451b5cf531146d42d815a1bd5bf347ea1bacb
AndersonAlvesCoelho <a-a-coelho-@hotmail.com>
2021-03-23 21:31:50 -0300

    fixed (serve sockte HTTP) SemanaJSExpert-3.0 - Dia 2

--- hacker-chat/client/index.js
+++ hacker-chat/client/index.js
@@ -1,9 +1,21 @@
+/**
+  node index.js \
+  --username andesonalves \
+  --room sala01 \
+  --hostUri localhost
+ */
+
+
+
 import Events from 'events';
+import CliConfig from './src/cliConfig.js';
 import TerminalConroller from "./src/terminalControlle.js";
 
+const [nodePath, filePath, ...commands] = process.argv;
+const config = CliConfig.parseArguments(commands);
+console.log('config', config);
 
 const componentEmitter = new Events();
-const controller = new TerminalConroller();
 
-
-await controller.initializeTable(componentEmitter);
+// const controller = new TerminalConroller();
+// await controller.initializeTable(componentEmitter);
